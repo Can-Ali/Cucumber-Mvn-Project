17df7d1174b0c5b38bc156f1b46491581af0151c
Can-Ali <can.ali73@gmail.com>
2023-10-18 20:58:26 +0100

    tag deleted in cucekrunner

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitons",
         dryRun = false, // true means that do not run my step definitions but only check if any step is missing snippet.
-        tags = "@employee"
+        tags = ""
 )
 
 public class CukesRunner {
